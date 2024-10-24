3251e8dd62bff1cf4e767167209b94560edccb1c
Alexandre Ferreira <86257031+xandynho96@users.noreply.github.com>
2024-10-24 12:11:11 -0300

    feat: changing for mongodb

--- server/index.js
+++ server/index.js
@@ -1,31 +1,57 @@
 import express from 'express';
 import cors from 'cors';
-import pkg from 'pg';
 import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
 
 dotenv.config();
 
-const { Pool } = pkg;
+// Configuração do MongoDB
+const mongoUri = process.env.MONGO_URI;
+let mongoClient;
+let mongoDb;
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const connectToMongoDB = async () => {
+    try {
+        mongoClient = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoClient.connect();
+        mongoDb = mongoClient.db('nome_do_banco'); // Substitua pelo nome do seu banco de dados no MongoDB
+        console.log('Conectado ao MongoDB');
+    } catch (error) {
+        console.error('Erro ao conectar ao MongoDB:', error);
+    }
+};
+
+connectToMongoDB();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/products', async (req, res) => {
+// Rota para criar um novo produto (POST)
+app.post('/api/mongo-products', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM products');
-        res.json(result.rows);
+        const newProduct = req.body; // Obter os dados do produto do corpo da requisição
+
+        // Validação simples para garantir que os campos obrigatórios existam
+        if (
+            !newProduct.id ||
+            !newProduct.name ||
+            !newProduct.description ||
+            !newProduct.price ||
+            !newProduct.image ||
+            !newProduct.proportion ||
+            typeof newProduct.proportion.coffee !== 'number' ||
+            typeof newProduct.proportion.milk !== 'number'
+        ) {
+            return res.status(400).json({ error: 'Faltando campos obrigatórios ou estrutura inválida' });
+        }
+
+        const productsCollection = mongoDb.collection('products'); // Nome da coleção no MongoDB
+        const result = await productsCollection.insertOne(newProduct);
+        res.status(201).json(result); // Retornar sucesso com o ID do novo produto
     } catch (error) {
-        console.error('Erro ao buscar produtos:', error);
-        res.status(500).json({ error: 'Erro ao buscar produtos' });
+        console.error('Erro ao criar produto no MongoDB:', error);
+        res.status(500).json({ error: 'Erro ao criar produto no MongoDB' });
     }
 });
 
