09cdbfd1fd739f3a2104c42eb8f5864f2e105dc2
Alexandre Ferreira <86257031+xandynho96@users.noreply.github.com>
2024-10-24 14:02:59 -0300

    adjustment

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,7 @@ export const connectToMongoDB = async () => {
     try {
         mongoClient = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
         await mongoClient.connect();
-        mongoDb = mongoClient.db('Cluster0'); // Nome do banco de dados
+        mongoDb = mongoClient.db('Cluster0'); 
         console.log('Conectado ao MongoDB');
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB:', error);
